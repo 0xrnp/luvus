d54af0e000cf31e5d9ad390270bf3ace63b714b5
RizRiyz <2667489+RizRiyz@users.noreply.github.com>
2026-08-14 15:50:11 +0800

    release: v0.10.2

--- nix/package.nix
+++ nix/package.nix
@@ -22,7 +22,7 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "bohay";
-  version = "0.10.1";
+  version = "0.10.2";
 
   # Required for new by-name packages (nixpkgs-vet NPV-166).
   __structuredAttrs = true;
